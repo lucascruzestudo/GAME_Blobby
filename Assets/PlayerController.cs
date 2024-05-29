d7a7c1d8e0ae47a60741ecbd32674c637148c534
lucas <lucascruzestudo@gmail.com>
2024-05-28 23:39:22 -0300

    feat: implementando parada de jogador após derrota

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
-    public float moveSpeed = 10;
+    public float moveSpeed = 10f;
     public Rigidbody2D rb;
 
     private float moveX;
+
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -17,12 +17,26 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        moveX = Input.GetAxis("Horizontal") * moveSpeed;
+        if (GameManager.Instance != null && !GameManager.Instance.gameEnded)
+        {
+            moveX = Input.GetAxis("Horizontal") * moveSpeed;
+        }
+        else
+        {
+            moveX = 0f; // Stop horizontal movement if the game has ended
+        }
     }
 
-    private void FixedUpdate(){
+    private void FixedUpdate()
+    {
         Vector2 velocity = rb.velocity;
         velocity.x = moveX;
         rb.velocity = velocity;
+    }
+
+    public void StopPlayer()
+    {
+        rb.velocity = Vector2.zero;
+        moveX = 0f;
     }
 }
